7081310d39efdf748a9b696b2b02f5eeae0b9646
Highlander Santos <santosray62@gmail.com>
2025-10-28 14:23:50 -0300

    let

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,30 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+// Create express instance
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server)
+  );
 
-   // Enable CORS
+  // Enable CORS
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: '*',
   });
 
-  
-   // For Vercel serverless
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.init();
 }
+
+// Initialize the app
 bootstrap();
+
+// Export for Vercel
+export default server;
